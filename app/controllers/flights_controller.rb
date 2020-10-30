6cf8ea77bb92cc7f06e2d74babe88d0a86eaf24c
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2020-10-29 17:34:23 -0700

    Flight search form shows search parameters with result. form_with now uses a model on @flight

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,14 +1,20 @@
 class FlightsController < ApplicationController
   def index
     # fail
+    @flight = params[:flight] ? Flight.new(search_params) : Flight.new
     if params[:flight]
-      @flights = Flight.where(flight_params).where('takeoff::date = ?', params[:date])
+      @flights = Flight.where(flight_params).where('takeoff::date = ?', params[:flight][:date])
     end
+    # fail
   end
 
   private
 
   def flight_params
-    params.require(:flight).permit(:origin_id, :destination_id, :passengers)
+    params.require(:flight).permit(:origin_id, :destination_id)
+  end
+
+  def search_params
+    params.require(:flight).permit(:origin_id, :destination_id, :date, :passengers)
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -11,10 +11,13 @@
 #  updated_at     :datetime         not null
 #
 class Flight < ApplicationRecord
+  attr_accessor :date, :passengers
+
   belongs_to :origin, class_name: :Airport
   belongs_to :destination, class_name: :Airport
 
   def takeoff_formatted
     takeoff.strftime("%m/%d/%Y")
   end
+
 end
