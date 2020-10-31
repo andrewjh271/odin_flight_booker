1176353812ad8d20c067699604eb6b8010deb0c2
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2020-10-30 23:30:04 -0700

    Refactored search_form and moved into partial

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,14 +1,18 @@
 class FlightsController < ApplicationController
   def index
-    # fail
     @flight = params[:flight] ? Flight.new(search_params) : Flight.new
     if params[:flight]
-      @flights = Flight.where(flight_params)
+      # removes fields user did not select (e.g. origin: '')
+      params[:flight].delete_if { |_k, v| v.empty? }
+      @flights = params[:flight].empty? ? Flight.all : Flight.where(flight_params)
     end
-    # fail
   end
 
   private
+
+  # search_params is designed to build the Flight object that will populate the search params
+  # on a redirect after a search (it includes :passengers which is not in the db table)
+  # flight_params is used to query the database
 
   def flight_params
     params.require(:flight).permit(:origin_id, :destination_id, :date)
